7880ab46389e4952fd25c9cdff236941e6f1b4c7
Kari Johnson <kari@crownmediamarketing.com>
2021-02-07 22:42:13 -0700

    uploaded icon images to icon folder

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// create variables
 let button = document.querySelector('.button')
 let inputValue = document.querySelector('.inputValue')
 let cityName = document.querySelector('.cityName')
 let date = document.querySelector('.date')
-let icon = document.querySelector('.icon')
 let temperature = document.querySelector('.temperature')
 let humidity = document.querySelector('.humidity')
 let uvIndexEl = document.querySelector('.uvIndex')
-
+let icon = document.querySelector('.icon')
+let windSpeed = document.querySelector('.windSpeed')
 
 button.addEventListener('click', function () {
     getTodaysWeather(inputValue.value)
 
 })
+
 function getTodaysWeather(searchValue) {
     fetch(
         'https://api.openweathermap.org/data/2.5/weather?q=' + searchValue + '&units=imperial&appid=f95318bd608cfefe9150d3d74bda27e7'
@@ -20,25 +20,30 @@ function getTodaysWeather(searchValue) {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            //temperate liter will need to go in all 3 functions for icons
-            $(".display").append(`
-           <p>${data.main.temp}</p> 
-            `)
-            //need to be the last things in the object
+            //temperate literals will need to go in all 3 functions for icons
+            $(".container").append(`
+           <p>${data.main.showTemp}</p> 
+           <p>${data.main.showCity}</p>
+           <p>${data.main.showDate}</p>
+           <p>${data.main.showHumidity}</p>
+           <p>${data.main.showUvIndex}</p>
+           <p>${data.main.showWindSpeed}</p>
+           <p>${data.main.icon}</p>
+           `)
+
             getUvIndex(data.coord.lat, data.coord.lon)
             getForecast(searchValue)
-          
+
         })
         .catch(err => alert("City not found, please try again"))
-
-
 }
+
 function getUvIndex(lat, lon) {
     fetch('http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=f95318bd608cfefe9150d3d74bda27e7')
         .then(response => response.json())
         .then(UvResponse => {
             console.log(UvResponse)
-           let uvIndex = UvResponse.value 
+            let uvIndex = UvResponse.value
         })
         .catch(err => alert("City not found, please try again"))
 
